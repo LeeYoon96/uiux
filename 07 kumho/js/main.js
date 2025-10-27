7be4e1379b383c3b9282026d0ed5e62cb23029fa
LeeYoon96 <tjth380312@gmail.com>
2025-10-27 18:06:05 +0900

    1

--- 07 kumho/js/main.js
+++ 07 kumho/js/main.js
@@ -35,6 +35,12 @@ $(document).ready(function(){
     const news_swiper = new Swiper('.news .swiper', { /* 팝업을 감싼는 요소의 class명 */
 	slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
 	spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+    scrollbar: {
+        el: ".news .ctrl_wrap .scrollbar",
+        hide: false,
+        draggable: true, //드래그 가능
+        dragSize: 'auto', //스크롤바 사이즈
+    },
 	breakpoints: {
 		640: {    /* 640px 이상일때 적용 */
 			slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
@@ -43,14 +49,12 @@ $(document).ready(function(){
         1025: {    /* 640px 이상일때 적용 */
 			slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
 			spaceBetween: 40,
+            scrollbar: {
+            dragSize: 222, //스크롤바 사이즈
+            },
 		},
 	},
-    scrollbar: {
-        el: ".news .ctrl_wrap .scrollbar",
-        hide: false,
-        draggable: true, //드래그 가능
-        dragSize: 222, //스크롤바 사이즈
-    },
+    
 });
 /*********************************************
      * .service .list ul li 에 마우스를 오버했을때
