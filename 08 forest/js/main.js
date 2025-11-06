3a6e3f65d809f5a7fce30e0ce9eb8901b1288130
LeeYoon96 <tjth380312@gmail.com>
2025-11-06 13:12:41 +0900

    08 forest_251106_visual

--- 08 forest/js/main.js
+++ 08 forest/js/main.js
@@ -25,17 +25,45 @@ $(document).ready(function(){
 		scrollOverflow: false, /* 컨텐츠가 넘쳐도 스크롤 금지 */
 
 		afterLoad: function(origin, destination, direction, trigger){
-			if((destination.index == 1) || (destination.index == 3) || (destination.index == 4)){ /* index가 1면 슬라이드는 두번째 슬라이드입니다. index 수는 0/1/2/3 */
-				// console.log('2번째 슬라이드가 로딩 되었을때');
-                $('header').addClass('dark')
+			if((destination.index == 1) || (destination.index == 3)){ /* index가 1면 슬라이드는 두번째 슬라이드입니다. index 수는 0/1/2/3 */
+				// console.log('나옴', trigger);
+				$('#fp-nav').removeClass('hide')
+				$('header').addClass('dark')
+				$('#fp-nav').addClass('dark')
+			}else if(destination.index == 4){
+				// console.log('여기는 footer')
+				$('#fp-nav').addClass('hide')
 			}else{
+				$('#fp-nav').removeClass('hide')
                 $('header').removeClass('dark')
+				$('#fp-nav').removeClass('dark')
             }
 		},
 
-		responsiveWidth: 768, /* fullpage를 적용시키지 않을 모바일 사이즈 */
+		responsiveWidth: 769, /* fullpage를 적용시키지 않을 모바일 사이즈 */
         responsiveWidth: 500
-	});
+	});//fullpage
+
+	const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+	autoplay: {  /* 팝업 자동 실행 */
+		delay: 5000,
+		disableOnInteraction: true,
+	},
+
+	//effect: "fade", /* fade 효과 */
+
+	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+	pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+		el: '.swiper-pagination', /* 해당 요소의 class명 */
+		clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+		type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+		renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+		    return '<span class="' + className + '">' + (index + 1) + "</span>";
+		},
+	},
+});
 
 
 
